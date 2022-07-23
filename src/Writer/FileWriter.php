b5d1dbd18423ad82d279111017218aacf64a6b07
sgolinski <simon.golinski@gmail.com>
2022-07-23 17:04:05 +0200

    fix

--- src/Writer/FileWriter.php
+++ src/Writer/FileWriter.php
@@ -7,12 +7,12 @@ class FileWriter implements Writer
 
     public static function writeTokensFromLastCronJob(array $makers): void
     {
-        file_put_contents('last_rounded_coins.txt', serialize($makers));
+        file_put_contents('/mnt/app/last_rounded_coins.txt', serialize($makers));
     }
 
     public static function writeTokensToListTokensAlreadyProcessed(array $makers): void
     {
-        file_put_contents('tokens_already_recorded.txt', serialize($makers));
+        file_put_contents('/mnt/app/tokens_already_recorded.txt', serialize($makers));
     }
     public static function writeOne(string $alert)
     {
